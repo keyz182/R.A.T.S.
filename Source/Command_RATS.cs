a5eebd9a2f927f9ff2ba327c41582d20cd5adaf0
Kieran David Evans <keyz182@gmail.com>
2024-06-23 11:39:40 +0100

    Icon fix

--- Source/Command_RATS.cs
+++ Source/Command_RATS.cs
@@ -4,11 +4,14 @@ using Verse;
 
 namespace RATS;
 
+[StaticConstructorOnStartup]
 public class Command_RATS : Command_VerbTarget
 {
     public Ability Ability;
     public Verb_AbilityRats Verb => (Verb_AbilityRats)Ability.verb;
     public Pawn Pawn;
+    
+    public static readonly Texture2D Tex = ContentFinder<Texture2D>.Get("UI/RATS_Logo-Small");
     
     public override bool Visible
     {
@@ -28,6 +31,7 @@ public class Command_RATS : Command_VerbTarget
         Ability = ability;
         Pawn = pawn;
         verb = Ability.verb;
+        icon = Tex;
     }
 
     public override Color IconDrawColor => defaultIconColor;
